81314b9664ceccda39e7d21f145f98677e8594e0
ruvick <rudikov.web@ya.ru>
2021-03-19 13:49:10 +0300

    Фото нет фото

--- 404.php
+++ 404.php
@@ -5,6 +5,6 @@
 	<div class = "content contentInPage centrElem">	
 		<h1 class = "h404">404</h1>
 		Запрашиваемая страница не была найдена
-
 	</div>
-<?php get_footer(); ?>
+
+<?php get_footer(); ?>  
